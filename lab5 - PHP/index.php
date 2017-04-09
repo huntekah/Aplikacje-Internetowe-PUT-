df99539ad222ded77c1ff176353f362dddaadb2d
Krzysztof Jurkiewicz <huntekah@gmail.com>
2017-04-09 12:48:28 +0200

    adding, removing and clearing basket works properly. Cannot buy products

--- lab5 - PHP/index.php
+++ lab5 - PHP/index.php
@@ -1,28 +1,48 @@
 ﻿<html>
-<meta charset="utf-8" />
+<meta charset="utf-8"/>
 <title>HunteShop</title>
 <script type="text/javascript"
         src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script type="text/javascript">
-    $(function (){
-        $('.buyProduct').click(function(){
+    $(function () {
+        $('.buyProduct').click(function () {
             var clickBtnValue = $(this).val(),
                 requestsUrl = 'requests.php',
                 data = {'addProduct': clickBtnValue};
-            $.post(requestsUrl,data,function(response){
+            $.post(requestsUrl, data, function (response) {
                 window.location.reload(true);
-                alert("You clicked button" + clickBtnValue + " to buy sth");
+                // alert("You clicked button" + clickBtnValue + " to buy sth");
             });
         });
 
-        $('.removeProduct').click(function(){
+        $('.removeProduct').click(function () {
             var clickBtnValue = $(this).val(),
                 requestsUrl = 'requests.php',
                 data = {'removeProduct': clickBtnValue};
-            $.post(requestsUrl,data,function(response){
+            $.post(requestsUrl, data, function (response) {
                 //alert(response);
                 window.location.reload(true);
-                alert("You clicked button" + clickBtnValue + " for removal");
+                //  alert("You clicked button" + clickBtnValue + " for removal");
+            });
+        });
+
+        $('.buyBasket').click(function () {
+            var requestsUrl = 'requests.php',
+                data = {'buyBasket': 1};
+            $.post(requestsUrl, data, function (response) {
+                window.location.reload(true);
+                if('price' in response) alert(response['price']);
+                if('phantom' in response){
+                    alert('you couldnt buy those products' + JSON.stringify(response['phantom']));
+                }
+            });
+        });
+
+        $('.clearBasket').click(function () {
+            var requestsUrl = 'requests.php',
+                data = {'clearBasket': 1};
+            $.post(requestsUrl, data, function (response) {
+                window.location.reload(true);
             });
         });
 
@@ -36,8 +56,8 @@
         <a href="#"><img src="koszyk.png" width="40" height="40"></a>
     </div>
 </header>
-<button>Kup</button>
-<button>wyfal</button>
+<button class="buyBasket">Kup</button>
+<button class="clearBasket">wyfal</button>
 <br>
 <?php
 session_start();
@@ -51,18 +71,16 @@ if ($db->connect_errno) {
 
 $db->begin_transaction();
 if ($result = $db->query("select * from product")) {
-    echo $result->num_rows;
 
 
-    if(!isset($_SESSION['basket'])) {
+    if (!isset($_SESSION['basket'])) {
         $_SESSION['basket'] = array();
     }
     $basket = &$_SESSION['basket'];
 
     print_r($basket);
     echo "<br>";
-    print_r($_SESSION['basket']);
-
+    echo "<div class='productList'>";
     echo "<table>";
     echo "<tr>";
     echo "<td>nazwa</td>";
@@ -77,15 +95,14 @@ if ($result = $db->query("select * from product")) {
         $price = $row[2];
         $description = $row[3];
         $img = $row[$IMG];
-        if(!in_array($id,$basket)) {
+        if (!in_array($id, $basket)) {
             echo "<tr class='active'><td>$name</td><td></td><td></td></tr>";
             echo "<tr class='active'>";
             echo "<td><img src=\"" . $img . "\" width=\"80\" height=\"80\"/></td>";
             echo "<td>$description</td>";
             echo "<td>$price</td>";
             echo "<td><button name='$name' value='$id' class='buyProduct'>Kup Teraz</button></td></tr>";
-        }
-        else{
+        } else {
             echo "<tr class='inactive'><td>$name</td><td></td><td></td></tr>";
             echo "<tr class='inactive'>";
             echo "<td><img src=\"" . $img . "\" width=\"80\" height=\"80\"/></td>";
@@ -93,19 +110,10 @@ if ($result = $db->query("select * from product")) {
             echo "<td>$price</td>";
             echo "<td><button name='$name' value='$id' class='removeProduct'>Wyjmij <br>z koszyka</button></td></tr>";
         }
-        // AJAX + PHP
 
-//
-//        foreach ($row as $key => $value) {
-//            if ($key == $IMG) {
-//                echo "<td><img src=\"" . $value . "\" width=\"80\" height=\"80\"/></td>";
-//
-//            } else {
-//                echo "<td>" . ($value) . "</td>";
-//            }
-//        }
     }
-echo "</table>";
+    echo "</table>";
+    echo "</div>";
 }
 $db->commit();
 $db->close();
--- lab5 - PHP/requests.php
+++ lab5 - PHP/requests.php
@@ -12,19 +12,79 @@ if (isset($_POST['addProduct'])) {
     addToBasket((int)$_POST['addProduct']);
 }
 
+if (isset($_POST['removeProduct'])) {
+    removeFromBasket((int)$_POST['removeProduct']);
+}
+
+if (isset($_POST['buyBasket'])) {
+    buyBasket();
+}
+
+if (isset($_POST['clearBasket'])) {
+    clearBasket();
+}
+
+
 function addToBasket($id)
 {
-    //$name = substr($_POST['addProduct'], 0, 6);
-    //$id = (int)substr($_POST['addProduct'], 6);
-    // add to global array of products and prepare for deletion.
-    $_SESSION['basket'][] = $id; // dodaje id do listy do listy
+    $_SESSION['basket'][] = $id;
     exit;
 }
 
-function insert()
+function removeFromBasket($id)
 {
-    echo "The insert function is called.";
+    $array = &$_SESSION['basket'];
+    array_splice($array, array_search($id, $array), 1);
+    //$_SESSION['basket'] = array_diff($_SERVER['basket'],array($id));
     exit;
 }
+
+//function buyBasket()
+//{
+//    $totalCost = 0;
+//    $basket = &$_SESSION['basket'];
+//    $db = new mysqli("localhost", "root", "", "inf122546");
+//    if ($db->connect_errno) {
+//        printf("Connect failed: %s\n", $db->connect_error);
+//        exit();
+//    }
+//    $db->begin_transaction();
+//
+//    if ($result = $db->query("select ID, name, price from product")) {
+//        while ($row = $result->fetch_row()) {
+//
+//            $id = $row[0];
+//            $price = $row[2];
+//
+//            if (array_search($id, $basket)) {
+//                if ($db->query("DELETE FROM product WHERE ID = " . $id)) {
+//                    array_splice($basket, array_search($id, $basket), 1);
+//                    $totalCost += $price;
+//                } else {
+//                    echo "There was an unexpected error while deleting an object with id = " . $id;
+//                }
+//            }
+//
+//        }
+//        $phantom_items = array();
+//        if (sizeof($basket) > 0) {
+//            $phantom_items['phantom'] = array(); // wierd, i know.
+//            foreach($basket as $itemID){
+//                $phantom_items['phantom'][] = $itemID;
+//            }
+//        }
+//        return (array_merge($phantom_items,array('price' => $totalCost));
+//
+//    }
+//
+//    exit;
+//}
+
+function clearBasket()
+{
+    $_SESSION['basket'] = array();
+    exit;
+}
+
 
 ?>
